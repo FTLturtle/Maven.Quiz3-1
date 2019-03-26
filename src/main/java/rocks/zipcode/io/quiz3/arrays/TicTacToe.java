60f421f40fd58d947be0f3e5a3ec8d6469f8f76c
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-25 20:48:23 -0400

    finished arrays

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -1,37 +1,76 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    private String[][] board;
 
     public TicTacToe(String[][] board) {
-    }
-
-    public TicTacToe() {
+        this.board = board;
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        return new String[]{board[0][value], board[1][value], board[2][value]};
+    }
+
+    private String[] getTLtoBRdiagonal() {
+        String[] diagonal = new String[3];
+        for (int i = 0; i < 3; i++) {
+            diagonal[i] = board[i][i];
+        }
+        return diagonal;
+    }
+
+    private String[] getBLtoTRdiagonal() {
+        String[] diagonal = new String[3];
+        for (int i = 0; i < 3; i++) {
+            diagonal[i] = board[i][2 - i];
+        }
+        return diagonal;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        return isStringArrayHomogeneous(getRow(rowIndex));
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        return isStringArrayHomogeneous(getColumn(columnIndex));
+    }
+
+    private Boolean isStringArrayHomogeneous(String[] stringArray) {
+        return (Arrays.stream(stringArray)
+                .distinct()
+                .toArray(String[]::new)
+                .length == 1);
     }
 
     public String getWinner() {
-        return null;
+        String winner = "tie";
+
+        if (isStringArrayHomogeneous(getBLtoTRdiagonal())) {
+            winner = getBLtoTRdiagonal()[0];
+        } else if (isStringArrayHomogeneous(getTLtoBRdiagonal())) {
+            winner = getTLtoBRdiagonal()[0];
+        } else {
+            for (int i = 0; i < 3; i++) {
+                if (isRowHomogenous(i)) {
+                    winner = getRow(i)[0];
+                } else if (isColumnHomogeneous(i)) {
+                    winner = getColumn(i)[0];
+                }
+            }
+        }
+        return winner;
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,32 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        StringBuilder sb = new StringBuilder(str.toLowerCase());
+        char ch;
+        ArrayList<String> result = new ArrayList<>();
+        boolean replaced;
+
+        for (int i = 0; i < str.length(); i++) {
+            replaced = false;
+            while (!replaced && i < str.length()) {
+                ch = sb.charAt(i);
+                if (Character.isAlphabetic(ch)) {
+                    sb.setCharAt(i, Character.toUpperCase(ch));
+                    result.add(sb.toString());
+                    sb.setCharAt(i, ch);
+                    replaced = true;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 }
