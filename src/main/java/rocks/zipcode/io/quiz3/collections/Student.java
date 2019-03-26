3ba6365f63a8c0b9ca6819a7c5f5c94a53003f84
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-25 20:53:07 -0400

    finished collections

--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,30 +1,61 @@
 package rocks.zipcode.io.quiz3.collections;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
 
+    private TreeMap<Lab, LabStatus> map;
+
     public Student() {
-        this(null);
+        this(new TreeMap<>());
     }
 
-    public Student(List<Lab> labs) {
+    public Student(Map<Lab, LabStatus> map) {
+        this.map = (TreeMap<Lab, LabStatus>) map;
     }
 
     public Lab getLab(String labName) {
+        for (Lab lab : map.keySet()) {
+            if (lab.getName().equals(labName)) {
+                return lab;
+            }
+        }
         return null;
     }
 
-    public void setLabStatus(String labName, LabStatus labStatus) {
+    public void setLabStatus(String labName, LabStatus labStatus) throws UnsupportedOperationException {
+        Lab lab = getLab(labName);
+
+
+        try {
+            map.get(lab);
+        } catch (NullPointerException n) {
+            throw new UnsupportedOperationException();
+        }
+
+        map.replace(lab, labStatus);
     }
 
+
     public void forkLab(Lab lab) {
+        map.put(lab, LabStatus.PENDING);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return map.get(getLab(labName));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Lab lab : map.keySet()) {
+            s.append(String.format("%s > %s\n", lab.getName(), map.get(lab).toString()));
+        }
+        return s.deleteCharAt(s.length() - 1).toString();
     }
 }
